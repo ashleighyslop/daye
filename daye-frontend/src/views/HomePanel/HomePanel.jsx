520b294293df7943c9c8cbb000cd971328834377
ashleighyslop <ahyslop2@sapient.com>
2020-01-19 00:21:30 +0000

    initial api call and display data from api in element

--- daye-frontend/src/views/HomePanel/HomePanel.jsx
+++ daye-frontend/src/views/HomePanel/HomePanel.jsx
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 
-export default () => (
-  <p>ashleigh</p>
-);
+const HomePanel = () => {
+
+  const [returnedData, updateData] = useState(undefined);
+
+  async function fetchData() {
+    let response = await fetch('https://front-end-test-bvhzjr6b6a-uc.a.run.app/');
+    let data = await response.json();
+    return data;
+  }
+
+  async function getData() {
+    const data = await fetchData();
+    updateData(data);
+  }
+
+  useEffect(() => {
+    getData();
+  }, [returnedData]);
+
+  return (
+    <div>
+      {Array.isArray(returnedData) && returnedData.map((item, idx) => <p key={idx}>{item.price}</p>)}
+    </div>
+  );
+};
+
+
+export default HomePanel;
--- daye-frontend/webpack.config.js
+++ daye-frontend/webpack.config.js
@@ -3,28 +3,8 @@ const path = require("path");
 const dotenv = require("dotenv");
 // const Dotenv = require("dotenv-webpack");
 
-const config = mode => {
-  console.log(mode);
-  let envKeys;
-  if (mode.ENVIRONMENT === "development") {
-    const env = dotenv.config({ path: "./.env" }).parsed;
-    envKeys = Object.keys(env).reduce((prev, next) => {
-      prev[`process.env.${next}`] = JSON.stringify(env[next]);
-      return prev;
-    }, {});
-  } else env = process.env;
-  envKeys = {
-    "process.env.REACT_APP_API_URL": JSON.stringify(
-      process.env.REACT_APP_API_URL
-    ),
-    "process.env.REACT_APP_API_KEY": JSON.stringify(
-      process.env.REACT_APP_API_KEY
-    ),
-    "process.env.REACT_APP_MOCK_USER_ID": JSON.stringify(
-      process.env.REACT_APP_MOCK_USER_ID
-    )
-  };
-  console.log(new webpack.DefinePlugin(envKeys));
+const config = () => {
+
   return {
     entry: ["react-hot-loader/patch", "./src/App.js"],
     output: {
@@ -50,7 +30,6 @@ const config = mode => {
     devServer: {
       contentBase: "./dist"
     },
-    plugins: [new webpack.DefinePlugin(envKeys)]
   };
 };
 
